9d24b90d060de0161944c5b3d1011370d8a34389
sai pavan Avasarala <pavan_avasarala@outlook.com>
2026-05-21 13:09:21 +0530

    added code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
@@ -8,7 +10,12 @@ def home():
 
 @app.route('/about')
 def about():
+    print("This is print statement by saipavan")
     return "This is the about page."
+
+@app.route("/newendpoint")
+def newendpoint():
+    return "success"
 
 if __name__ == '__main__':
     app.run(debug=True)
